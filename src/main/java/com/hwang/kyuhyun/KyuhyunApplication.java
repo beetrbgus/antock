e0a17dfe12cb06980ec274a8b16da7770a298000
beetrbgus <devbeetrb0214@gmail.com>
2025-04-03 04:08:42 +0900

    Initial commit

---
+++ src/main/java/com/hwang/kyuhyun/KyuhyunApplication.java
@@ -0,0 +1,13 @@
+package com.hwang.kyuhyun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyuhyunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyuhyunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hwang/kyuhyun/KyuhyunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwang.kyuhyun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyuhyunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
