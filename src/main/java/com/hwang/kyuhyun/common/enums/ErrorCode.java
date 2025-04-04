1a4f75d48ca307269236fde93a9156029c79d910
beetrbgus <devbeetrb0214@gmail.com>
2025-04-04 21:02:36 +0900

    [FEATURE] 커스텀 에러 코드 추가

--- src/main/java/com/hwang/kyuhyun/common/enums/ErrorCode.java
+++ src/main/java/com/hwang/kyuhyun/common/enums/ErrorCode.java
@@ -11,10 +11,13 @@ import java.util.Arrays;
 public enum ErrorCode {
 
     INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "잘못된 입력값입니다."),
+    EXTERNAL_SERVER_ERROR(HttpStatus.BAD_REQUEST, "Csv 파일을 읽는 도중 문제가 발생했습니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다."),
     METHOD_ARGUMENT_TYPE_MISMATCH(HttpStatus.BAD_REQUEST, "잘못된 파라미터 타입입니다."),
     NOT_FOUND_END_POINT(HttpStatus.NOT_FOUND, "존재하지 않는 API입니다."),
     TOO_FAST_REQUEST(HttpStatus.TOO_MANY_REQUESTS, "이전 요청을 처리하고 있습니다. 10초 후 다시 시도해주세요"),
+    NOT_FOUNT_LABEL(HttpStatus.BAD_REQUEST, "해당 통신판매업 컬럼이 존재하지 않습니다."),
+    CVS_NOT_VALIDATED(HttpStatus.INTERNAL_SERVER_ERROR, "유효한 CVS 파일이 아닙니다."),
     ;
 
     private final HttpStatus httpStatus;
