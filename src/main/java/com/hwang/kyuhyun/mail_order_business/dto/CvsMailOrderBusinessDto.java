c2714bda2b38abe15463ec1a4839e702c62964e2
beetrbgus <devbeetrb0214@gmail.com>
2025-04-06 08:38:07 +0900

    [FEATURE] 통신 판매업 DTO 수정
    - 생성자 수정 (CVS 파일에 '010 처럼 '가 붙은 경우가 있음)
    - 주소에 공백 제거

--- src/main/java/com/hwang/kyuhyun/mail_order_business/dto/CvsMailOrderBusinessDto.java
+++ src/main/java/com/hwang/kyuhyun/mail_order_business/dto/CvsMailOrderBusinessDto.java
@@ -11,58 +11,41 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class CvsMailOrderBusinessDto {
-    // 통신판매번호
-    private String mailOrderNumber;
-    // 신고기관명
-    private String reportAgencyName;
-    // 상호
-    private String businessName;
-    // 사업자등록번호
-    private String businessRegistrationNo;
-    // 법인여부
-    private String isCorporation;
-    // 대표자명
-    private String ceoName;
-    // 전화번호
-    private String phoneNumber;
-    // 전자우편
-    private String email;
-    // 신고일자
-    private String reportDate;
-    // 사업장소재지
-    private String location;
-    // 사업장소재지(도로명)
-    private String roadLocation;
-    // 업소상태
-    private String businessStatus;
-    // 신고기관 대표연락처
-    private String reportAgencyContact;
-    // 판매방식
-    private String salesMethod;
-    // 취급품목
-    private String itemsHandled;
-    // 인터넷도메인
-    private String domain;
-    // 호스트서버소재지
-    private String hostServerLocation;
+    private String mailOrderNumber; // 통신판매번호
+    private String reportAgencyName; // 신고기관명
+    private String businessName; // 상호
+    private String businessRegistrationNo; // 사업자등록번호
+    private String isCorporation; // 법인여부
+    private String ceoName; // 대표자명
+    private String phoneNumber; // 전화번호
+    private String email; // 전자우편
+    private String reportDate; // 신고일자
+    private String location; // 사업장소재지
+    private String roadLocation; // 사업장소재지(도로명)
+    private String businessStatus; // 업소상태
+    private String reportAgencyContact; // 신고기관 대표연락처
+    private String salesMethod; // 판매방식
+    private String itemsHandled; // 취급품목
+    private String domain; // 인터넷도메인
+    private String hostServerLocation; // 호스트서버소재지
 
-    public CvsMailOrderBusinessDto(String[] infos) {
-        this.mailOrderNumber = infos[0];
-        this.reportAgencyName = infos[1];
-        this.businessName = infos[2];
-        this.businessRegistrationNo = infos[3].replaceAll("-", "");
-        this.isCorporation = infos[4];
-        this.ceoName = infos[5];
-        this.phoneNumber = infos[6];
-        this.email = infos[7];
-        this.reportDate = infos[8];
-        this.location = infos[9];
-        this.roadLocation = infos[10];
-        this.businessStatus = infos[11];
-        this.reportAgencyContact = infos[12];
-        this.salesMethod = infos[13];
-        this.itemsHandled = infos[14];
-        this.domain = infos[15];
-        this.hostServerLocation = infos[16];
+    public CvsMailOrderBusinessDto(String[] line) {
+        this.mailOrderNumber = line[0];
+        this.reportAgencyName = line[1];
+        this.businessName = line[2];
+        this.businessRegistrationNo = line[3].replaceAll("-", "");
+        this.isCorporation = line[4];
+        this.ceoName = line[5];
+        this.phoneNumber = line[6];
+        this.email = line[7];
+        this.reportDate = line[8];
+        this.location = line[9];
+        this.roadLocation = line[10].replaceAll(" ", "");
+        this.businessStatus = line[11];
+        this.reportAgencyContact = line[12];
+        this.salesMethod = line[13];
+        this.itemsHandled = line[14];
+        this.domain = line[15];
+        this.hostServerLocation = line[16];
     }
 }
