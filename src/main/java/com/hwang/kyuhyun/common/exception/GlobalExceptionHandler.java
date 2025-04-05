3b0d46e0b70b422c7343e7566e511d0366102770
beetrbgus <devbeetrb0214@gmail.com>
2025-04-06 02:04:12 +0900

    [FEATURE] Exception Handler 추가
    - 로깅 추가
    - ResponseEntity 반환하게 수정

--- src/main/java/com/hwang/kyuhyun/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/hwang/kyuhyun/common/exception/GlobalExceptionHandler.java
@@ -2,25 +2,37 @@ package com.hwang.kyuhyun.common.exception;
 
 import com.hwang.kyuhyun.common.enums.ErrorCode;
 import com.hwang.kyuhyun.common.response.CommonResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import java.io.IOException;
-
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(AppCustomException.class)
     public ResponseEntity<CommonResponse<Void>> handleException(AppCustomException e) {
         CommonResponse<Void> response = CommonResponse.fail(e.getErrorCode());
-
+        log.error("AppCustomException message : {}", e.getMessage());
         return ResponseEntity
                 .status(ErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus())
                 .body(response);
     }
 
+    @ExceptionHandler(value = {MethodArgumentNotValidException.class , BindException.class})
+    public ResponseEntity<CommonResponse<Void>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("GlobalExceptionHandler message : {}", e.getMessage());
+        return ResponseEntity
+                .status(ErrorCode.INVALID_INPUT_VALUE.getHttpStatus())
+                .body(CommonResponse.fail(ErrorCode.INVALID_INPUT_VALUE));
+    }
     @ExceptionHandler(Exception.class)
-    public CommonResponse<Void> handleException(Exception e) {
-        return CommonResponse.fail(ErrorCode.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<CommonResponse<Void>> handleException(Exception e) {
+        log.error("Exception message : {}", e.getMessage());
+        return ResponseEntity
+                .status(ErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus())
+                .body(CommonResponse.fail(ErrorCode.INTERNAL_SERVER_ERROR));
     }
 }
